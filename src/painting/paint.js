2cd044dc1ac6ff5ddf2d9fcdb246871a98016804
JohnChen <paiching01@gmail.com>
2020-03-24 21:01:29 +0800

    e

--- src/painting/paint.js
+++ src/painting/paint.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+    function getRandomColor(){
+        let color='#';
+        let color2=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+        for(let i=0;i<7;i++){
+            console.log('E');
+
+        }
+    }
+
+
+
 
     let paint;
     $('.color').on('click',function () {
@@ -8,9 +19,5 @@ $(document).ready(function () {
     $('.paint').on('click',function () {
         this.style.backgroundColor = paint;
     })
-
-
-
-
 
 })
